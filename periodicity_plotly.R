8f215dbb9e28150a859e4f977019b9144e00c1a9
pauline.francois <pauline.francois@i2bc.paris-saclay.fr>
2019-04-18 13:55:49 +0200

    Ajout d'un graphe pour représenter le gff

--- periodicity_plotly.R
+++ periodicity_plotly.R
@@ -27,8 +27,26 @@ for(i in 1:nrow(count)){
 count<-cbind(count,couleur)
 
 
-plot_ly(count, x = ~V1, y = ~V2, type = 'bar', 
+p <- plot_ly(count, x = ~V1, y = ~V2, type = 'bar', 
              marker = list(color= ~couleur, line = list(width = 0))) %>%
   layout(title = "Periodicity",
          xaxis = list(title = "Position"),
          yaxis = list(title = "Count"))
+
+p2 <- plot_ly()   %>%
+  layout(yaxis = list(title = 'Annotation', 
+                      fixedrange=T,
+                      range = c(-1.5,1.5),
+                      zeroline = T,
+                      showline = T,
+                      showticklabels = T,
+                      showgrid = T),
+         xaxis = list( range = c(file$V1[1], file$V1[nrow(file)]),
+                       title = "Position",
+                       zeroline = T,
+                       showline = T,
+                       showticklabels = T,
+                       showgrid = F)) 
+
+
+subplot(p, p2, shareX=TRUE,nrows = 2, heights = c(0.8, 0.2))
