91b25f5ed5e59f5687fd9fb5a6f3dd9a88875185
pauline.francois <pauline.francois@i2bc.paris-saclay.fr>
2019-04-18 11:41:08 +0200

    Ajout de la couleur

--- periodicity_plotly.R
+++ periodicity_plotly.R
@@ -2,8 +2,33 @@ library(plotly)
 
 count<-read.table("/partage/bioinfo/VIH/run/qualitativeAnalysis/periodicity_vih/VIH.merge.count2.-.txt")
 
+couleur<-NULL
+for(i in 1:nrow(count)){
+	un<-(count$V1[i]-1)
+	deux<-(count$V1[i]-2)
+	if(count$V1[i]==1) {
+		couleur<-c(couleur,"green")
+	} 
+	else if(count$V1[i]==2) {
+		couleur<-c(couleur,"red")
+	} 
+	else if(count$V1[i]==3) {
+		couleur<-c(couleur,"blue")
+	}
+	else if(un%%3==0) {
+		couleur<-c(couleur,"green")
+	}
+	else if(deux%%3==0) {
+		couleur<-c(couleur,"red")
+	} 
+	else
+		couleur<-c(couleur,"blue")
+}
+count<-cbind(count,couleur)
+
+
 plot_ly(count, x = ~V1, y = ~V2, type = 'bar', 
-             marker = list(line = list(width = 0))) %>%
+             marker = list(color= ~couleur, line = list(width = 0))) %>%
   layout(title = "Periodicity",
          xaxis = list(title = "Position"),
          yaxis = list(title = "Count"))
