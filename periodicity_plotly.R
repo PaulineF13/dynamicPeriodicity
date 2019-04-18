e59864ad6041c9c3403284aa78e16c3e7c97e110
pauline.francois <pauline.francois@i2bc.paris-saclay.fr>
2019-04-18 14:40:12 +0200

    Ajout de la phase et de la couleur du gff

--- periodicity_plotly.R
+++ periodicity_plotly.R
@@ -26,6 +26,51 @@ for(i in 1:nrow(count)){
 }
 count<-cbind(count,couleur)
 
+gff <- read.table("/partage/MEMBERS/pauline.francois/database/gencode.v29.annotation.apprisPrincipale.gff3",skip = 7, sep="\t",
+                nrows = 15, header = F)
+
+stage<-NULL
+color<-NULL
+for (i in 1:nrow(gff)){
+	un<-(gff$V4[i]-1)
+	deux<-(gff$V4[i]-2)
+	if(gff$V1[i]==1) {
+		phase<-1
+	} 
+	else if(gff$V1[i]==2) {
+		phase<-2
+	} 
+	else if(count$V1[i]==3) {
+		phase<-3
+	}
+	else if(un%%3==0) {
+		phase<-1
+	}
+	else if(deux%%3==0) {
+		phase<-2
+	} 
+	else
+		phase<-3
+
+	if(gff$V7[i]=="-"){
+		color<-c(color,"blue")
+		if(phase==1){
+			phase<-3
+		}
+		else if (phase==3){
+			phase<-1
+		}
+	}
+	else{
+		color<-c(color,"red")
+	}
+
+	stage<-c(stage,phase)
+}
+
+gff<-cbind(gff,stage)
+gff<-cbind(gff,color)
+
 
 p <- plot_ly(count, x = ~V1, y = ~V2, type = 'bar', 
              marker = list(color= ~couleur, line = list(width = 0))) %>%
