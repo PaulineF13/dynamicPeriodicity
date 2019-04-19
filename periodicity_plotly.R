70ff4714914bae0e9aeb496c5a8110a7fe46edbb
pauline.francois <pauline.francois@i2bc.paris-saclay.fr>
2019-04-19 11:14:13 +0200

    Préparation du script pour passer en R mark Down

--- periodicity_plotly.R
+++ periodicity_plotly.R
@@ -1,82 +1,93 @@
 library(plotly)
 
-count<-read.table("/partage/bioinfo/VIH/run/qualitativeAnalysis/periodicity_vih/VIH.merge.count2.-.txt")
+#count<-read.table("/partage/bioinfo/VIH/run/qualitativeAnalysis/periodicity_vih/VIH.merge.count2.+.txt")
+count<-read.table(params$data)
 
 couleur<-NULL
 for(i in 1:nrow(count)){
-	un<-(count$V1[i]-1)
-	deux<-(count$V1[i]-2)
-	if(count$V1[i]==1) {
-		couleur<-c(couleur,"green")
-	} 
-	else if(count$V1[i]==2) {
-		couleur<-c(couleur,"red")
-	} 
-	else if(count$V1[i]==3) {
-		couleur<-c(couleur,"blue")
-	}
-	else if(un%%3==0) {
-		couleur<-c(couleur,"green")
-	}
-	else if(deux%%3==0) {
-		couleur<-c(couleur,"red")
-	} 
-	else
-		couleur<-c(couleur,"blue")
+  un<-(count$V1[i]-1)
+  deux<-(count$V1[i]-2)
+  if(count$V1[i]==1) {
+    couleur<-c(couleur,"green")
+  } 
+  else if(count$V1[i]==2) {
+    couleur<-c(couleur,"red")
+  } 
+  else if(count$V1[i]==3) {
+    couleur<-c(couleur,"blue")
+  }
+  else if(un%%3==0) {
+    couleur<-c(couleur,"green")
+  }
+  else if(deux%%3==0) {
+    couleur<-c(couleur,"red")
+  } 
+  else
+    couleur<-c(couleur,"blue")
 }
 count<-cbind(count,couleur)
 
-gff <- read.table("/partage/MEMBERS/pauline.francois/database/gencode.v29.annotation.apprisPrincipale.gff3",skip = 7, sep="\t",
-                nrows = 15, header = F)
-
-stage<-NULL
-color<-NULL
-for (i in 1:nrow(gff)){
-	un<-(gff$V4[i]-1)
-	deux<-(gff$V4[i]-2)
-	if(gff$V1[i]==1) {
-		phase<-1
-	} 
-	else if(gff$V1[i]==2) {
-		phase<-2
-	} 
-	else if(count$V1[i]==3) {
-		phase<-3
-	}
-	else if(un%%3==0) {
-		phase<-1
-	}
-	else if(deux%%3==0) {
-		phase<-2
-	} 
-	else
-		phase<-3
-
-	if(gff$V7[i]=="-"){
-		color<-c(color,"blue")
-		if(phase==1){
-			phase<-3
-		}
-		else if (phase==3){
-			phase<-1
-		}
-	}
-	else{
-		color<-c(color,"red")
-	}
-
-	stage<-c(stage,phase)
+#gff <- read.table("/partage/MEMBERS/pauline.francois/database/vih.annotation.referenceGenome.gff3", sep="\t", header = F)
+gff<-read.table(params$gff3,sep="\t",header=F)
+annot<-params$annotation
+if(annot=="CDS"){
+  gff_soft<-subset(gff,gff$V3=="CDS")
+} else if(annot=="gene"){
+  gff_soft<-subset(gff,gff$V3=="gene")
+} else if(annot=="five_prime_UTR and three_prime_UTR"){
+  gff_soft<-subset(gff,gff$V3=="five_prime_UTR" | gff$V3=="three_prime_UTR")
+} else if(annot=="transcript"){
+  if("transcript" %in% gff$V3){
+    gff_soft<-subset(gff,gff$V3=="transcript")
+  }
+  else{
+    gff_soft<-subset(gff,gff$V3=="five_prime_UTR" | gff$V3=="three_prime_UTR" | gff$V3=="CDS")
+  }
+} else{
+  gff_soft<-gff
 }
 
-gff<-cbind(gff,stage)
-gff<-cbind(gff,color)
+stage<-NULL
+for (i in 1:nrow(gff_soft)){
+  un<-(gff_soft$V4[i]-1)
+  deux<-(gff_soft$V4[i]-2)
+  if(gff_soft$V1[i]==1) {
+    phase<-1
+  } 
+  else if(gff_soft$V1[i]==2) {
+    phase<-2
+  } 
+  else if(gff_soft$V1[i]==3) {
+    phase<-3
+  }
+  else if(un%%3==0) {
+    phase<-1
+  }
+  else if(deux%%3==0) {
+    phase<-2
+  } 
+  else
+    phase<-3
+  
+  if(gff_soft$V7[i]=="-"){
+    if(phase==1){
+      phase<-3
+    }
+    else if (phase==3){
+      phase<-1
+    }
+  }
+  
+  stage<-c(stage,phase)
+}
 
+gff_soft<-cbind(gff_soft,stage)
 
 p <- plot_ly(count, x = ~V1, y = ~V2, type = 'bar', 
              marker = list(color= ~couleur, line = list(width = 0))) %>%
   layout(title = "Periodicity",
          xaxis = list(title = "Position"),
-         yaxis = list(title = "Count"))
+         yaxis = list(title = "Count", range = c(0,2000)))
 
 p2 <- plot_ly()   %>%
   layout(yaxis = list(title = 'Annotation', 
@@ -86,47 +97,50 @@ p2 <- plot_ly()   %>%
                       showline = F,
                       showticklabels = F,
                       showgrid = F),
-         xaxis = list( range = c(file$V1[1], file$V1[nrow(file)]),
+         xaxis = list( range = c(count$V1[1], count$V1[nrow(count)]),
                        title = "Position",
                        zeroline = T,
                        showline = T,
                        showticklabels = T,
                        showgrid = F)) 
-for(i in 1:nrow(gff)){
-	if(gff$stage[i]==1){
-		p2 <- add_trace(p2, 
-                  x = c(gff$V4[i]/10,gff$V5[i]/10),
-                  y = rep(1,2) ,
-                  type="scatter",
-                  mode = 'lines+markers',
-                  text = gsub(";","\n",gff$V9[i]) ,
-                  hoverinfo = 'text',
-                  showlegend = F,
-                  marker = list(color= ~color, width = 4)
-  		)
-	}
-	else if(gff$stage[i]==2)	
-  		p2 <- add_trace(p2, 
-                  x = c(gff$V4[i]/10,gff$V5[i]/10),
-                  y = rep(0,2) ,
-                  type="scatter",
-                  mode = 'lines+markers',
-                  text = gsub(";","\n",gff$V9[i]) ,
-                  hoverinfo = 'text',
-                  showlegend = F,
-                  marker = list(color= ~color, width = 4)
-        )
- 	else if(gff$stage[i]==3)	
-  		p2 <- add_trace(p2, 
-                  x = c(gff$V4[i]/10,gff$V5[i]/10),
-                  y = rep(-1,2) ,
-                  type="scatter",
-                  mode = 'lines+markers',
-                  text = gsub(";","\n",gff$V9[i]) ,
-                  hoverinfo = 'text',
-                  showlegend = F,
-                  marker = list(color= ~color, width = 4)
-        )
+for(i in 1:nrow(gff_soft)){
+  if(gff_soft$stage[i]==1){
+    p2 <- add_trace(p2, 
+                    x = c(gff_soft$V4[i],gff_soft$V5[i]),
+                    y = rep(1,2) ,
+                    type="scatter",
+                    mode = 'lines+markers',
+                    text = gsub(";","\n",gff_soft$V9[i]) ,
+                    hoverinfo = 'text',
+                    showlegend = F,
+                    marker = list(color= "green", width = 4),
+                    line=list(color="green")
+    )
+  }
+  else if(gff_soft$stage[i]==2) 
+    p2 <- add_trace(p2, 
+                    x = c(gff_soft$V4[i],gff_soft$V5[i]),
+                    y = rep(0,2) ,
+                    type="scatter",
+                    mode = 'lines+markers',
+                    text = gsub(";","\n",gff_soft$V9[i]) ,
+                    hoverinfo = 'text',
+                    showlegend = F,
+                    marker = list(color= "red", width = 4),
+                    line=list(color="red")
+    )
+  else if(gff_soft$stage[i]==3) 
+    p2 <- add_trace(p2, 
+                    x = c(gff_soft$V4[i],gff_soft$V5[i]),
+                    y = rep(-1,2) ,
+                    type="scatter",
+                    mode = 'lines+markers',
+                    text = gsub(";","\n",gff_soft$V9[i]) ,
+                    hoverinfo = 'text',
+                    showlegend = F,
+                    marker = list(color= "blue", width = 4),
+                    line=list(color="blue")
+    )
 }
 
 subplot(p, p2, shareX=TRUE,nrows = 2, heights = c(0.8, 0.2))
